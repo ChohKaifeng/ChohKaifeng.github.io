b93e817085358e5f47a3df96bb69b9915ecaa321
ChohKaifeng <chohkfeng32@gmail.com>
2020-06-15 14:51:22 +0800

    Added AlienEyesFollower

---
+++ javascript/AlienEyesFollow/AlienEyesFollow.js
@@ -0,0 +1,10 @@
+document.querySelector('body').addEventListener('mousemove', eyeMovement);
+function eyeMovement() {
+  var eye = document.querySelector('.eye');
+  let x = eye.getBoundingClientRect().left;
+  let y = eye.getBoundingClientRect().top;
+  let radian = Math.atan2(event.pageX - x, event.pageY - y);
+  console.log(x);
+  let rot = radian * (180 / Math.PI) * -1;
+  eye.style.transform = 'rotate(' + rot + 'deg)';
+}
